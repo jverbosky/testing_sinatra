c99cd853f9d184d6ed54484863ebdeae59924eb8
jverbosk <jverbosk@gmail.com>
2017-02-23 15:16:58 -0500

    tried multiple variations of variable in hidden field to pass it along, ultimately looked at Max and Matt's examples of putting redirect value in form action

--- tests/tests_app.rb
+++ tests/tests_app.rb
@@ -42,15 +42,15 @@ class TestApp < Minitest::Test  # TestApp subclass inherits from Minitest::Test 
     assert(last_response.body.include?('John'))  # two ways to pass assertion - see notes in lines 31- 36
   end
 
-  # def test_get_age
-  #   get '/age', u_name: 'jv'  # seed value - not an assertion, corresponds to backend_name_2 = params[:u_name]
-  #   # get '/age?user_name=jv'  # variation of previous line since we're using a redirect
-  #   assert(last_response.ok?)  # for "get 'get_age' do", need to retrieve something from the server to pass
-  #   assert(last_response.body.include?('jv'))  # reminder - body != erb body, see notes in lines 31 - 36
-  #   assert(last_response.body.include?('What is your age?'))
-  #   assert(last_response.body.include?('<form method="post" action="age">'))
-  #   assert(last_response.body.include?('<input type="text" name="user_age">'))
-  # end
+  def test_get_age
+    # get '/age', u_name: 'JV'  # seed value - not an assertion, corresponds to backend_name_2 = params[:u_name]
+    get '/age?u_name=JV'  # variation of previous line since we're using a redirect
+    assert(last_response.ok?)  # for "get 'get_age' do", need to retrieve something from the server to pass
+    # assert(last_response.body.include?('JV'))  # reminder - body != erb body, see notes in lines 31 - 36
+    assert(last_response.body.include?('Hello JV, what is your age?'))
+    assert(last_response.body.include?('<form method="post" action="post_age?user_n=JV'))
+    assert(last_response.body.include?('<input type="text" name="user_a">'))
+  end
 
   # def test_post_age_multiple_values_no_redirects  # app.rb line 31 uncommented
   #   post '/post_age', user_n: 'john_v', user_a: '41'  # passing multiple values
